c64ee04cb31a6488dea298c6da480dbef6da5ae0
RedPanda210 <league@WTS21.attlocal.net>
2017-08-06 15:27:54 -0700

    what2

---
+++ src/day3/HelloWorld.java
@@ -0,0 +1,10 @@
+package day3;
+
+import javax.swing.JOptionPane;
+
+public class HelloWorld {
+public static void main(String[] args) {System.out.println("Sup World");
+String name=JOptionPane.showInputDialog("Enter Your Name");
+JOptionPane.showMessageDialog(null, name+" why is my house gone."); }
+
+}
--- src/day3/WorldDomination.java
+++ src/day3/WorldDomination.java
@@ -5,11 +5,12 @@ import javax.swing.JOptionPane;
 public class WorldDomination {
 	public static void main(String[] args) {
 		// 1. Ask the user if they know how to write code.
-
-		// 2. If they say "yes", tell them they will rule the world.
-
+String awnser=JOptionPane.showInputDialog("Do you have the skills to code");
+if(awnser.equals("yes")){JOptionPane.showMessageDialog(null,"then we will rule the world" );
+	}
+else{		// 2. If they say "yes", tell them they will rule the world.
+JOptionPane.showMessageDialog(null, " Nooob, wash my dishes.");
 		// 3. Otherwise, wish them good luck washing dishes.
-
+}
 	}
 }
-
